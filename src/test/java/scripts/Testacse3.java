b59b6dad8ec59e683649089c2ce2b084daea5a94
QSP <QSP@QSP>
2021-12-12 09:33:31 +0530

    Jenkins

---
+++ src/test/java/scripts/Testacse3.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testacse3 {
+	@Test
+	public void tc3() {
+		Reporter.log("Hello Python",true);
+	}
+
+}
