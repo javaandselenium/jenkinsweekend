c37117ea73d5e6df6caef37da4babf094734bde7
QSP <QSP@QSP>
2021-12-12 10:01:55 +0530

    Hii

---
+++ src/test/java/scripts/Testacse5.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testacse5 {
+	@Test
+	public void tc5() {
+		Reporter.log("Hello Postman",true);
+	}
+
+}
---
+++ src/test/java/scripts/Testacse6.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testacse6 {
+	@Test
+	public void tc6() {
+		Reporter.log("Hello Restassured",true);
+	}
+
+}
